cd6be851e2693c1f4b5963295c2b2a5003a200bf
Pablo Gonzalez <pabgon0@gmail.com>
2020-05-01 08:03:53 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,10 @@
 class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
+  
+  def actors_list
+    self.characters.map do |c|
+      "#{c.actor.}"
+    end
+  end
 end
